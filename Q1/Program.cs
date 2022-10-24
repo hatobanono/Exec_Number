115524414366c7d59b47b269070e22800dd4e59a
hatobanono <chris00iq@gmail.com>
2022-10-24 17:26:03 +0800

    complete

---
+++ Q1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("請輸入數值: ");
+			string input = Console.ReadLine();
+
+			int num = Convert.ToInt32(input);
+
+			if (num == 2 || num == 3)
+			{
+				Console.WriteLine($"{num} 是質數");
+			} else if (num % 2 == 0)
+			{
+				Console.WriteLine($"{num} 不是質數");
+			}
+			else
+			{
+				int int1 = 0;
+				for (int i = 3; i <= Math.Sqrt(num); i += 2)
+				{
+					if (num % i == 0)
+					{
+						int1++;
+						break;
+					}
+				}
+
+				string result = (int1 == 1) ? $"{num} 不是質數" : $"{num} 是質數";
+				Console.WriteLine(result);
+			}
+
+
+		}
+	}
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int start = 2;
+			int end = 1000;
+
+			for (int i = start; i <= end; i++)
+			{
+				int m = 0;
+				for(int k =2; k <=Math.Sqrt(i); k++)
+				{
+					if (i % k == 0)
+					{
+						m++;
+						break;
+					}
+				}
+				if (m == 0)
+				{
+					Console.WriteLine(i);
+				}
+			} 
+		}
+	}
+}
